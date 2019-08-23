21840fc2bd52fe6100bcf6685a73977f12521e15
Sree Polavarapu <sree.polavarapu@amp.co.nz>
2019-08-23 17:38:50 +1200

    Req #first_tests - Added basic UI and widget test

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class FirstPage extends StatelessWidget {
+class AnotherPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
--- lib/routes.dart
+++ lib/routes.dart
@@ -3,13 +3,13 @@ import 'package:flutter_tests/first_screen.dart';
 import 'package:flutter_tests/main.dart';
 
 class Routes {
-  static const HOME_SCREEN_ROUTE = '/home_screen';
-  static const FIRST_SCREEN_ROUTE = '/first_screen';
+  static const HOME_SCREEN_ROUTE = '/';
+  static const ANOTHER_SCREEN_ROUTE = '/another_screen';
 
   static Map<String, WidgetBuilder> routes() {
     return <String, WidgetBuilder>{
-      FIRST_SCREEN_ROUTE: (context) => FirstPage(),
-      HOME_SCREEN_ROUTE: (context) => HomePage()
+      ANOTHER_SCREEN_ROUTE: (context) => AnotherPage(),
+      HOME_SCREEN_ROUTE: (context) => HomePage(title: "Home Screen",)
     };
   }
 }
